423ecda00595909610e151288e4309f885222066
Your Name <samanbatool08@gmail.com>
2020-10-31 23:01:22 -0400

    fetching item data

--- src/Items.js
+++ src/Items.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 function Items() {
+
+    function fetchItems(){
+        fetch('http://localhost:4000/items')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
     return (
-        <div>
+        <div className='items'>
+            <div className='items__list'>
+                {fetchItems()}
+            </div>
             
         </div>
     )
