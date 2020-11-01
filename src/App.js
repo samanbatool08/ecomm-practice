d873287b9352cfceae7d47e8528a7d47cd58ab75
Your Name <samanbatool08@gmail.com>
2020-10-31 21:58:10 -0400

    component heirarchy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Header from './Header';
+import Items from './Items';
 
 function App() {
   return (
     <div className="app">
       <div className='app__headline'>
       <h1> Saman's store </h1>
+      <Header />
+      <Items />
       </div>
     </div>
   );
