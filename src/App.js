58b4ce8d6245d84fd2fb523ce94c4b963d1842b1
Your Name <samanbatool08@gmail.com>
2020-10-31 22:01:35 -0400

    header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,8 @@ import Items from './Items';
 function App() {
   return (
     <div className="app">
-      <div className='app__headline'>
-      <h1> Saman's store </h1>
       <Header />
       <Items />
-      </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
 
 function Header() {
     return (
         <div className='header'>
-            
+            <div className='header__headline'>
+            <h1>Saman's store</h1>
+            </div>
         </div>
     )
 }
