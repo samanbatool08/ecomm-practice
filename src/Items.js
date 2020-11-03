cb22f1730b17e0859d9740708c56a53fd8bfc541
Your Name <samanbatool08@gmail.com>
2020-11-03 12:41:09 -0500

    state management

--- src/Items.js
+++ src/Items.js
@@ -1,27 +1,51 @@
 import React from 'react'
 
-function Items() {
+class Items extends React.Component {
 
-    function fetchItems(){
+    state = {
+        items: {}
+    }
+
+    
+    // function displayItem(item) {
+    //     return (
+    //         <div>
+    //             <h2>Name: {item.name}</h2>
+    //             <h2>Image: {item.image}</h2>
+    //             <h2>Description: {item.description}</h2>
+    //             <h2>Price: {item.price}</h2>
+    //         </div>
+    //     )
+    // }
+
+    
+     fetchItems() {
         fetch('http://localhost:4000/items')
         .then(response => response.json())
-        .then(items => {
-            console.log(items)
-            items.map(item => (
-                <div>
-                    <h2>Name: item.name</h2>
-                </div>
-            ))
+        .then(data => {
+            // console.log(data)
+            this.setState({items: data })
+            //  items.map(item => (
+            //    displayItem(item)
+            // ))
         })
     }
+    
+    render() {
+       
+        console.log(this.state)
+
+
     return (
         <div className='items'>
-            <div className='items__list'>
-                {fetchItems()}
+            <div className='items__list' style={{color: "red"}}>
+                HELLO
+                {this.fetchItems}
             </div>
             
         </div>
     )
+}
 }
 
 export default Items;
